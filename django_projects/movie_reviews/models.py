745ecc8877002ca2a5157ea4cea83c62ce6b4582
Gary Chung <gchungsy@gmail.com>
2024-03-15 17:11:00 -0700

    Add movie model to admin

--- django_projects/movie_reviews/models.py
+++ django_projects/movie_reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Movies(models.Model):
+class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie_reviews/images/')
