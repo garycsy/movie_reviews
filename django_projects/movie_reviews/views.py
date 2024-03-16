0b3fb68c2eec5d3f79c60902885f8810479f5117
Gary Chung <gchungsy@gmail.com>
2024-03-15 17:49:48 -0700

    Implement search to filter cards by title with a case-insensitive match

--- django_projects/movie_reviews/views.py
+++ django_projects/movie_reviews/views.py
@@ -3,10 +3,13 @@ from .models import Movie
 
 
 def home(request):
-    searchMovie = request.GET.get("searchMovie")
-    movies = Movie.objects.all()
+    searchTerm = request.GET.get("searchMovie")
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
 
-    return render(request, "home.html", {"searchTerm": searchMovie, 'movies': movies})
+    return render(request, "home.html", {"searchTerm": searchTerm, 'movies': movies})
 
 def signup(request):
     email = request.GET.get("email")
