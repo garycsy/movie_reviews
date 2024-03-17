772885958dc354fbe51c1861195c99b2b8560ee1
Gary Chung <gchungsy@gmail.com>
2024-03-17 01:13:36 -0700

    Add base template

--- django_projects/django_projects/settings.py
+++ django_projects/django_projects/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_projects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'movie_reviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
