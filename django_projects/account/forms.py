951a24fc4fb90e1922febea31b8846adb8e56c1e
Gary Chung <gchungsy@gmail.com>
2024-03-24 07:59:08 -0700

    update html style

--- django_projects/account/forms.py
+++ django_projects/account/forms.py
@@ -7,7 +7,7 @@ class UserCreateForm(UserCreationForm):
 
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update({'class': 'mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '})
+            self.fields[fieldname].widget.attrs.update({'class': 'input input-bordered flex items-center gap-2'})
 
 
 class UserLoginForm(AuthenticationForm):
@@ -17,4 +17,4 @@ class UserLoginForm(AuthenticationForm):
 
         for fieldname in ['username', 'password']:
             self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update({'class': 'mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '})
+            self.fields[fieldname].widget.attrs.update({'class': 'input input-bordered flex items-center gap-2'})
