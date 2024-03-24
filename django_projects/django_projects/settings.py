a2b2bb9d0d1426f1eb17b336c3bf3ea959b5643d
Gary Chung <gchungsy@gmail.com>
2024-03-24 10:15:58 -0700

    setup for pythonanywhere

--- django_projects/django_projects/settings.py
+++ django_projects/django_projects/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-llxzysln%vqr0j%)3o=tdrl22n99^uo58$_p6q41pvpysgv5si
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
